64bf6519c78aa85e0ad48ab974c96494b8f0bf3c
David Zombar <davidzombar@gmail.com>
2021-06-18 16:12:46 -0400

    add JavaScript element

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
 
-document.querySelector('body').appendChild(h2); 
+function addingEventListener() {
+    const mouseOver = document.getElementById('mouseOverEvent');
+    function mouseOverAlert() { alert("Thank you for visiting our page!")}
+    mouseOver.addEventListener('mouseover', mouseOverAlert)
+}
+
+function showAlert() {
+    alert ("Thank you for visiting my page!");
+  }
