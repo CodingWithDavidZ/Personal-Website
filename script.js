fe1e57750aff9b49b779f449dd9b9bf581c8e848
David Zombar <davidzombar@gmail.com>
2021-06-21 10:17:50 -0600

    Add visit counter to webpage

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
-function addingEventListener() {
-    const mouseOver = document.getElementById('mouseOverEvent');
-    function mouseOverAlert() { alert("Thank you for visiting our page!")}
-    mouseOver.addEventListener('mouseover', mouseOverAlert)
+//function addingEventListener() {
+//    const mouseOver = document.getElementById('mouseOverEvent');
+//    function mouseOverAlert() { alert("Thank you for visiting our page!")}
+//    mouseOver.addEventListener('mouseover', mouseOverAlert)
+//}
+
+//function showAlert() {
+//    alert ("Thank you for visiting my page!");
+//  }
+
+function pageVisitCounter(){
+    fetch('https://api.countapi.xyz/update/CodingWithDavidZ/JourneyOfUs/?amount=1')
+    .then(function(res) {res.json()})
+    .then(function(data) { counterElement.innerHTML = (data.value) }}
 }
 
-function showAlert() {
-    alert ("Thank you for visiting my page!");
-  }
+pageVisitCounter()
+
+counterElement= document.getElementsByClassName('count')[0]
