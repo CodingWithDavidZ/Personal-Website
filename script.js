3083c94f6e5034bb8fe1f93b6f8733d2337aba3a
David Zombar <davidzombar@gmail.com>
2021-06-21 11:27:25 -0600

    removed unneeded code

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-
-//function addingEventListener() {
-//    const mouseOver = document.getElementById('mouseOverEvent');
-//    function mouseOverAlert() { alert("Thank you for visiting our page!")}
-//    mouseOver.addEventListener('mouseover', mouseOverAlert)
-//}
-
-//function showAlert() {
-//    alert ("Thank you for visiting my page!");
-//  }
-
 function viewsCall(response){
     document.getElementById('views').innerText = response.value;
 }
